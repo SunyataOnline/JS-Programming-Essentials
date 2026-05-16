0c63b07855c8a1e103e8fa5d8ae78194d0cacab6
Sunyata <jakob.lucas1440@gmail.com>
2026-05-15 21:53:27 -0400

    Added followers_count

---
+++ followers_count/followers_count.js
@@ -0,0 +1,17 @@
+let count = 0; // Initialize count to 0
+
+function displayCount() {
+document.getElementById('countDisplay').innerHTML=count; // Display the count in the HTML
+}
+
+function checkCountValue() {
+  if (count % 10 == 0 && count > 0) {
+    alert("Your Instagram post gained " + count + " followers! Congratulations!");
+  }
+}
+
+  function increaseCount() {
+    count++; // Increment the count by 1
+    displayCount();
+    checkCountValue();
+  }
